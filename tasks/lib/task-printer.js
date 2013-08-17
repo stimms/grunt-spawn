d5d9bdd8f924e16dd70e6d03d97d99dbb85fd0ad
fir3pho3nixx <fir3pho3nixx@gmail.com>
2013-08-17 17:16:59 +0100

    Added a task printer

---
+++ tasks/lib/task-printer.js
@@ -0,0 +1,22 @@
+require("./include");
+
+function TaskPrinter(taskArgs) {
+
+	var self = this;
+
+	self.formatCmd = function(){
+		var stringArgs = _(taskArgs.args).toString();
+		var formattedArgs = S(stringArgs).replaceAll(",", " ");
+		return "{0} {1}".format(taskArgs.cmd, formattedArgs);
+	};
+
+	self.printCmd = function(){
+		var cmd = self.formatCmd();
+		grunt.log.subhead("\nSpawn: Launching child process");
+		grunt.log.ok("Spawn: Launching '{0}'".format(cmd));
+	};
+
+
+}
+
+module.exports = TaskPrinter;
