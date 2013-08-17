c407fec61f5129628620a63a49c056efc5eac3af
fir3pho3nixx <fir3pho3nixx@gmail.com>
2013-08-17 17:13:33 +0100

    Refactored task command

--- tasks/lib/task-command.js
+++ tasks/lib/task-command.js
@@ -6,39 +6,53 @@ function TaskCommand(taskArgs) {
 	self.cmd = taskArgs.cmd;
 	self.args = taskArgs.args;
 
-	self.execute = function(done) {
+	self.formatCmd = function(){
+		var stringArgs = _(taskArgs.args).toString();
+		var formattedArgs = S(stringArgs).replaceAll(",", " ");
+		return "{0} {1}".format(taskArgs.cmd, formattedArgs);
+	};
 
-		grunt.log.debug("spawn::lib::Task::#execute() ->");
+	self.printCmd = function(){
+		var cmd = self.formatCmd();
+		grunt.log.subhead("\nSpawn: Launching child process");
+		grunt.log.ok("Spawn: Launching '{0}'".format(cmd));
+	};
 
-		var options = {
+	self.createOpts = function(){
+		return {
 			cmd: taskArgs.cmd,
 			args: taskArgs.args,
 			nodeSpawnOptions: {
 				stdio: "inherit"
 			}
 		};
+	};
 
-		var formattedCmd = taskArgs.cmd;
-		var formattedArgs = S(_(taskArgs.args).toString()).replaceAll(",", " ");
-		var spawnCommand = "{0} {1}".format(formattedCmd, formattedArgs);
-
-		grunt.log.subhead("\nSpawn: Launching child process");
-		grunt.log.ok("Spawn: Launching '{0}'".format(spawnCommand));
-
-		var spawn = grunt.util.spawn(options, function() {});
+	self.redirectIO = function(spawn){
 		spawn.stdin.pipe(process.stdin);
 		spawn.stdout.pipe(process.stdout);
 		spawn.stderr.pipe(process.stderr);
+	};
+
+	self.execute = function(done) {
+		grunt.log.debug("spawn::lib::Task::#execute() ->");
+
+		self.printCmd();
+
+		var opts = self.createOpts();
+		var spawn = grunt.util.spawn(opts, function() {});
+		self.redirectIO(spawn);
 
 		spawn.on("error", function() {
 			grunt.fail.fatal("Spawn: A child process generated error. Exiting.", 1);
 		})
 
 		spawn.on("exit", function(code) {
+			if (done != null) 
+				done(code);
 
-			if (done != null) done(code);
-
-			if (code !== 0) grunt.fail.fatal("Spawn: A child process generated error. Exiting.", 1);
+			if (code !== 0) 
+				grunt.fail.fatal("Spawn: A child process generated error. Exiting.", 1);
 		})
 
 		grunt.log.debug("spawn::lib::Task::#execute() <-");
